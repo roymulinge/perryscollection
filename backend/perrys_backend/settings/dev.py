ce8a91a49020fc9ee192f41b06f46b43b42282cf
Roy <mutuaroymulinge@gmail.com>
2026-05-02 01:36:57 +0300

    feat:setup django

---
+++ backend/perrys_backend/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOST = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
---
+++ backend/perrys_backend/settings/prod.py
@@ -0,0 +1,26 @@
+from .base import *
+import os
+
+DEBUG = False
+
+#Actual domain when deployed
+ALLOWED_HOSTS =[]
+
+
+#PostgreSQL configuration via environment variables
+DATABASES ={
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+
+#Security settings for production
+SECURE_HSTS_SECONDS = 31536000
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
