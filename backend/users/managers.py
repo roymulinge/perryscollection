bf2b3acc00ac695f4ebc8d12b71477fd8fac44bd
Roy <mutuaroymulinge@gmail.com>
2026-05-03 14:00:06 +0300

    fix(user-manager):remove user.save

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -16,7 +16,7 @@ class CustomUserManager(BaseUserManager):
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
-        user.save
+        
         return user
     
     def create_superuser(self, email, password=None, **extra_fields):
