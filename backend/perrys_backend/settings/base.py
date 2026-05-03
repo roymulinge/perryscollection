58b2b400b0c7f86209840ec38d359a4a18d3b497
Roy <mutuaroymulinge@gmail.com>
2026-05-03 13:12:16 +0300

    feat(settings):config users app in settings and config the custom user

--- backend/perrys_backend/settings/base.py
+++ backend/perrys_backend/settings/base.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    #Apps
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
