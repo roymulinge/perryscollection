70db50c76415c4ffe2a0926f4428defd8b487d9b
Roy <mutuaroymulinge@gmail.com>
2026-05-03 13:06:54 +0300

    feat(users-models):Create a CustomUser  model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from .managers import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=150, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, default='')
+    phone_number_required = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    full_name = models.CharField(max_length=255, blank=True, default='')
+    is_shop_owner = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    def get_full_name(self):
+        return self.full_name or self.email
+    
+    def get_short_name(self):
+        return self.email
+
