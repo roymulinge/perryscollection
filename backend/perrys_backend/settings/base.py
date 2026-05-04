854701d046a464bb2e16e587b2ee84cd495cadbf
Roy <mutuaroymulinge@gmail.com>
2026-05-04 09:51:37 +0300

    feat(settings-base):add allauth for database models allauth.accounts, allauth.socialaccount and allauth.socialaccount.providers.google

--- backend/perrys_backend/settings/base.py
+++ backend/perrys_backend/settings/base.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     
     #Apps
     'users',
