a688ee9fae961eff9e6b4d7322b4ad921443beeb
Roy <mutuaroymulinge@gmail.com>
2026-05-04 14:23:14 +0300

    feat(users):Add a customSignupForm with phone_number as the field

---
+++ backend/users/forms.py
@@ -0,0 +1,16 @@
+from allauth.account.forms import SignupForm
+from django import forms
+
+class CustomSignupForm(SignupForm):
+    phone_number = forms.CharField(
+        max_legth = 15,
+        required = True,
+        label = "Phone number"
+    )
+
+    def save(self, request):
+        user = super().save(request)
+        user.phone_number = self.cleaned_data['phone_number']
+        user.phone_number_required = True
+        user.save()
+        return user
