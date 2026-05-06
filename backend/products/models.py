13736306713490fcaca8350bab2f64bde20871ae
Roy <mutuaroymulinge@gmail.com>
2026-05-06 20:02:52 +0300

    feat(products): create product model and its attribute

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    featured = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('products:product_detail', args=[self.slug])
+
+class Category(models.Model):
+    """"""
