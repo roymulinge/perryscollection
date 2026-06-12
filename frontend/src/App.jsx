7d75650cb76f8f1a48e34f08417a020f33c4c5d3
Roy <mutuaroymulinge@gmail.com>
2026-06-12 21:18:07 +0300

    update app.jsx wrapping the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
+import ProductsPage from './pages/ProductsPage'
 import NavBar from './components/NavBar'
 
 function App() {
-  
-
   return (
     <>
-     <HomePage/>
-     <NavBar/>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </>
   )
 }
