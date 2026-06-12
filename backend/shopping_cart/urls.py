3f215601daf4bbcd3952726f5482a2f1d674fcd4
Roy <mutuaroymulinge@gmail.com>
2026-06-12 19:38:17 +0300

    fix: correct app_name namespace from 'shopping-cart' to 'shopping_cart'

--- backend/shopping_cart/urls.py
+++ backend/shopping_cart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'shopping-cart'
+app_name = 'shopping_cart'
 
 urlpatterns = [
     path('', views.CartAPIView.as_view(), name='cart-detail'),
